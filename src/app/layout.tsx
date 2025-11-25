6a9ee2756122ae5f0fb5a05fdb466468e9274efd
Sakon Chankhachon <acidrainpsu1980@gmail.com>
2025-11-25 08:08:29 +0700

    chore: Update Leaflet CSS import from `@import` in `globals.css` to a CDN link in `layout.tsx`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="th" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+      </head>
       <body className={kanit.className}>
         {children}
       </body>
